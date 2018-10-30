2b8a9c3b9ea8046df583cce49963002711339796
Audra Stafursky <audrastafursky@Anitas-MacBook-Pro.local>
2018-10-30 08:43:26 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,47 @@
+#import the pandas module and name it pd for easy access
+
+#1. load in the data
+#1a. Save filename to variable
+
+#1b. Load filename into dataframe
+
+#2. Check it out
+#2a. View dataframe
+
+
+#2b. Only view first few entries
+
+
+#2c. View the first 10 entries
+
+
+#2d. View the last few entries
+
+
+#2e. View the last 10 entries
+
+
+#2f. View the number of rows and columns
+
+
+#2g. View the dialogue column
+
+
+#2h. View the 10th row
+
+
+#2i. View unique values and counts for a column (series)
+#2i1. Get a column
+
+
+#2i2. Get unique values and counts
+
+
+#2i3. Interpret results: Many of these entries have count 1 because they are more than just a character name.  Let's assume character name is in all caps. Extract all uppercase letters from the string using a list comprehension
+
+#We didn't talk about this in class, but see if you can figure out how to nest two list comprehensions to get rid of the outer for loop.  
+
+
+
+
+#2i4 Now that we have a cleaned list of names: remove duplicates, count occurances of each name, and print the top 10 speakers
